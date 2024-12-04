bde939d0aadfcab6290628857301b93ef0cb80f1
Miguel Ramos <return.infox@gmail.com>
2024-12-04 10:12:20 -0300

    controlador

---
+++ src/main/java/com/kibernumacademy/aop/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.kibernumacademy.aop.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kibernumacademy.aop.model.User;
+import com.kibernumacademy.aop.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+    ResponseEntity.created(null)
+    return ResponseEntity.ok(userService.addUser(user));
+  }
+
+  public ResponseEntity<List<User>> getAllUsers() {
+    return ResponseEntity.ok(userService.getUsers());
+  }
+
+  
+}
